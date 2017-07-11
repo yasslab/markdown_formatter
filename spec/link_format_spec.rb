799690f022044bb7c51715308a3fe9eb7e26bec5
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-07-11 10:45:31 +0900

    Update link format spec

--- spec/link_format_spec.rb
+++ spec/link_format_spec.rb
@@ -20,12 +20,53 @@ RSpec.describe MarkdownFormatter do
       expect(MarkdownFormatter.format(text)).to eq(expect)
     end
 
+    # FIXME: Do pass this spec.
+    xit "link with empty name" do
+      text, expect =<<~'TEXT', <<~'EXPECT'
+        [link]()
+
+        [link](<>)
+      TEXT
+        [link]()
+
+        [link](<>)
+      EXPECT
+
+      expect(MarkdownFormatter.format(text)).to eq(expect)
+    end
+
     it "link with newline" do
       text, expect =<<~'TEXT', <<~'EXPECT'
         [`localhost`](
         http://localhost)
+
+        [home
+        page](http://localhost)
       TEXT
         [`localhost`](http://localhost)
+
+        [home page](http://localhost)
+      EXPECT
+
+      expect(MarkdownFormatter.format(text)).to eq(expect)
+    end
+
+    it "whitespace is allowed around the destination and title" do
+      text, expect =<<~'TEXT', <<~'EXPECT'
+        [link](   /uri
+          "title"  )
+      TEXT
+        [link](/uri   "title"  )
+      EXPECT
+
+      expect(MarkdownFormatter.format(text)).to eq(expect)
+    end
+
+    it "not allowed between the link text and the following parenthesis" do
+      text, expect =<<~'TEXT', <<~'EXPECT'
+        [link] (/uri)
+      TEXT
+        [link] (/uri)
       EXPECT
 
       expect(MarkdownFormatter.format(text)).to eq(expect)
