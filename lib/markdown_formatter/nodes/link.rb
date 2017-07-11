3c9760379dd8511721a447c15459efc1471f3117
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-07-11 10:46:47 +0900

    Fix link format style

--- lib/markdown_formatter/nodes/link.rb
+++ lib/markdown_formatter/nodes/link.rb
@@ -2,7 +2,7 @@ module MarkdownFormatter
   module ASTNode
     class Link < Base
       def to_s
-        node.dig(:options, :raw_text).gsub(/\R/, '')
+        node.dig(:options, :raw_text).gsub(/\]\(\s*/, "](").gsub(/\R/, " ")
       end
     end
   end
